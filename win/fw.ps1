b231a1ae8006a50bd72f845487d89608372eeab3
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 16:32:47 -0700

    Update fw.ps1

--- win/fw.ps1
+++ win/fw.ps1
@@ -1,1 +1,8 @@
+$port = Read-Host "Port"
+$proto = Read-Host "Proto"
 
+New-NetFirewallRule -DisplayName "Block IN" -Direction Inbound -Action Block -Profile Any
+New-NetFirewallRule -DisplayName "Block OUT" -Direction Outbound -Action Block -Profile Any
+
+New-NetFirewallRule -DisplayName $port -Direction Inbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
+New-NetFirewallRule -DisplayName $port -Direction Outbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
