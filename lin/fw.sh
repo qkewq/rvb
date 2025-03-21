3ea200148778f6a9d3a06d12deecba5a33f5d9cf
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 16:25:47 -0700

    Update fw.sh

--- lin/fw.sh
+++ lin/fw.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+read -p "Port:" port
+read -p "Proto:" proto
 
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+
+iptables -A INPUT ip $proto --dport $port -j ACCEPT
+iptables -A OUTPUT ip $proto --sport $port -j ACCEPT
