5c44a4c989087cca0d79bf11e82ce25b8189286b
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 21:04:16 -0700

    Update changepw.ps1

--- win/changepw.ps1
+++ win/changepw.ps1
@@ -1,1 +1,8 @@
+$pw = Read-Host "Password"
+$secpw = ConvertTo-SecureString $pw -AsPlainText -Force
 
+$names = @("johncyberstrike", "joecyberstrike", "janecyberstrike", "janicecyberstrike", "strikesavior", "planetliberator", "haunterhunter", "vanguardprime", "roguestrike", "falconpunch", "specter", "antiterminite")
+
+foreach($name in $names){
+  Set-ADAccountPassword -Identity $name -NewPassword $secpw -Reset
+}
