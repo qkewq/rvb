7b985091e8924453e67790bd45e14c250593688a
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 18:26:10 -0700

    Update changepw.sh

--- lin/changepw.sh
+++ lin/changepw.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+read "Password: " pw
 
+names=("root" "johncyberstrike" "joecyberstrike" "janecyberstrike" "janicecyberstrike" "strikesavior" "planetliberator" "haunterhunter" "vanguardprime" "roguestrike" "falconpunch" "specter" "antiterminite")
+for (( i=0 ; i<13 ; i++ ));
+do
+  echo ${names[i]}:$pw | sudo chpasswd
+done
