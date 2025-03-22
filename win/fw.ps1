ae7d69e86b4d71a00636f8304eb7287b6cebffc4
kwood <155027796+qkewq@users.noreply.github.com>
2025-03-21 20:54:26 -0700

    Update fw.ps1

--- win/fw.ps1
+++ win/fw.ps1
@@ -1,8 +1,12 @@
+$opt = Read-Host "1 to block all, 2 to open port"
 $port = Read-Host "Port"
 $proto = Read-Host "Proto"
 
-New-NetFirewallRule -DisplayName "Block IN" -Direction Inbound -Action Block -Profile Any
-New-NetFirewallRule -DisplayName "Block OUT" -Direction Outbound -Action Block -Profile Any
-
-New-NetFirewallRule -DisplayName $port -Direction Inbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
-New-NetFirewallRule -DisplayName $port -Direction Outbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
+if($opt -eq "1"){
+  New-NetFirewallRule -DisplayName "Block IN" -Direction Inbound -Action Block -Profile Any
+  New-NetFirewallRule -DisplayName "Block OUT" -Direction Outbound -Action Block -Profile Any
+}
+elseif($opt -eq "2"){
+  New-NetFirewallRule -DisplayName $port -Direction Inbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
+  New-NetFirewallRule -DisplayName $port -Direction Outbound -Protocol $proto -LocalPort $port -Action Allow -Profile Any
+}
